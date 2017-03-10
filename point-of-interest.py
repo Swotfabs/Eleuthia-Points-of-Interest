660ca11e60ad3ff1ffb82df70911c86e22aa670c
Fabian Junge <fabsjunge@gmail.com>
2017-03-10 15:51:33 -0600

    Docstrings for Point of Interest

--- point-of-interest.py
+++ point-of-interest.py
@@ -1,1 +1,22 @@
-## Point of Interest Class
+"""Holds the PointOfInterest class and the InvalidPoint exception"""
+
+class InvalidPointException(Exception):
+    """This exception is thrown when an invalid point of interest is created"""
+
+    pass
+
+class PointOfInterest:
+    """Contains all of the information of an Eleuthia Point of Interest
+
+    Has the grades: 'unknown', 'normal', 'epic', 'deadly', and 'forbidden'
+    """
+
+    self.grades = {'unknown', 'normal', 'epic', 'deadly', 'forgidden'}
+
+    def __init__ (self, name, grade="unknown", solved=False)
+        """Constructs a Point of Interest.
+
+        It is given the grade 'unknown' and marked as unsolved if those are
+        not specificed
+        """
+        assert False, "Not Yet Implemented"
