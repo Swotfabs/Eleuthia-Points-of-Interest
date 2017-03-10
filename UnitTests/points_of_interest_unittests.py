a2839940ee5eac553ed0dddb09adb023825ebb9f
Fabian Junge <fabsjunge@gmail.com>
2017-03-10 17:26:38 -0600

    renaming things!

--- UnitTests/points_of_interest_unittests.py
+++ UnitTests/points_of_interest_unittests.py
@@ -1,5 +1,9 @@
-"""Runs unit tests on Points of Interests"""
+"""Runs unit tests on Points of Interests
+
+This should be run from the main folder (or via unittesting.py)
+"""
 import unittest
+import point_of_interest
 
 
 class PointOfIntersetTests(unittest.TestCase):
