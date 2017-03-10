2998d59280847f6c93009356367e62696a826f6c
Fabian Junge <fabsjunge@gmail.com>
2017-03-10 16:35:47 -0600

    created skeleton of Player class

--- player.py
+++ player.py
@@ -1,1 +1,50 @@
-## Player Class
+"""Holds the Player class which has points of interest"""
+
+
+class Player:
+    """Contains Points of Interest belonging to the player in sorted containers
+
+    """
+
+    def __init__(self, name):
+        """Creates a Player"""
+        assert False, "Not Yet Implemented"
+
+    def addPointOfInterest(self, point):
+        """Adds a point of interest to the player
+
+        This must be an instance of the PointOfInterest class.
+        """
+        assert False, "Not Yet Implemented"
+
+    def removePointOfInterest(self, point):
+        """Removes a point of interest from the player.
+
+        This does not have to be an instant of the POI class.
+        """
+
+        assert False, "Not Yet Implemented"
+
+    def markSolved(self, point):
+        """Marks a Point of Interest as solved
+
+        This does not have to be an instance of POI class.
+        """
+
+        assert False, "Not Yet Implemented"
+
+    def markUnsolved(self, point):
+        """Marks a Point of Interest as unsolved
+
+        This does not have to be an instance of POI class.
+        """
+
+        assert False, "Not Yet Implemented"
+
+    def changeDifficulty(self, point):
+        """Changes the difficulty of a Point of Interest
+
+        This does not have to be an instance of POI class.
+        """
+
+        assert False, "Not Yet Implemented"
