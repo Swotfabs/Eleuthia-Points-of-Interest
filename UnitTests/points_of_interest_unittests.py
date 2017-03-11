a9061236848b7dc42b22e9c5856b55875d770061
Fabian Junge <fabsjunge@gmail.com>
2017-03-10 18:19:11 -0600

    Point of Interest Unit Tests

--- UnitTests/points_of_interest_unittests.py
+++ UnitTests/points_of_interest_unittests.py
@@ -3,8 +3,55 @@
 This should be run from the main folder (or via unittesting.py)
 """
 import unittest
-import point_of_interest
+from point_of_interest import PointOfInterest, InvalidPointException
 
 
 class PointOfIntersetTests(unittest.TestCase):
-    pass
+    """Tests whether the PointOfInterest class is working"""
+
+    def setUp(self):
+        self.point = PointOfInterest("Test")
+
+    def test_default(self):
+        self.assertEqual(self.point.name, "default")
+        self.assertEqual(self.point.difficulty, 'unknown')
+        self.assertFalse(self.point.solved)
+
+    def test_create_custom(self):
+        pointTwo = PointOfInterest("TestTwo", 'normal', True)
+        self.assertEqual(pointTwo.name, "TestTwo")
+        self.assertEqual(pointTwo.difficulty, 'normal')
+        self.assertTrue(pointTwo.solved)
+
+    def test_create_invalid(self):
+        with self.assertRaises(TypeError):
+            pointTwo = PointOfInterest("TestTwo", 'INVALID', True)
+
+    def test_solve(self):
+        self.point.markSolved()
+        self.assertTrue(self.point.solved)
+
+    def test_unsolve(self):
+        self.point.markSolved()
+        self.point.markUnsolved()
+        self.assertFalse(self.point.solved)
+
+    def test_double_solve(self):
+        self.point.markSolved()
+        with self.assertRaises(AttributeError):
+            self.point.markSolved()
+
+    def test_double_unsolved(self):
+        with self.assertRaises(AttributeError):
+            self.point.markUnsolved()
+
+    def test_changeDifficulty(self):
+        self.point.changeDifficulty('epic')
+        self.assertEqual(self.point.difficulty, 'epic')
+
+    def test_changeDifficulty_invalid(self):
+        with self.assertRaises(TypeError):
+            self.point.changeDifficulty('INVALID')
+
+if __name__ == '__main__':
+    unittest.main()
--- point_of_interest.py
+++ point_of_interest.py
@@ -1,37 +1,40 @@
 """Holds the PointOfInterest class and the InvalidPoint exception"""
 
 
-class InvalidPointException(Exception):
-    """This exception is thrown when an invalid point of interest is created"""
-
-    pass
-
-
 class PointOfInterest:
     """Contains all of the information of an Eleuthia Point of Interest
 
     Has the difficulties: 'unknown', 'normal', 'epic', 'deadly',
-    and 'forbidden'
+        and 'forbidden'
     """
 
-    self.difficulties = {'unknown', 'normal', 'epic', 'deadly', 'forgidden'}
+    self.difficulties = {'unknown', 'normal', 'epic', 'deadly', 'forbidden'}
 
     def __init__(self, name, difficulty="unknown", solved=False):
         """Constructs a Point of Interest.
 
         It is given the difficulty 'unknown' and marked as unsolved if those
-        are not specificed
+        are not specificed.
+
+        Trying to create a Point of Interest with an invalid difficulty
+        raises a TypeError.
         """
         assert False, "Not Yet Implemented"
 
     def markSolved(self):
-        """Marks a Point of Interest as solved"""
+        """Marks a Point of Interest as solved.
+
+        Marking a solved point as solved raises AttributeError."""
         assert False, "Not Yet Implemented"
 
     def markUnsolved(self):
-        """Marks a Point of Interest as unsolved"""
+        """Marks a Point of Interest as unsolved.
+
+        Marking an unsolved point as unsolved raises AttributeError."""
         assert False, "Not Yet Implemented"
 
     def changeDifficulty(self, difficulty):
-        """Changes the difficulty of the Point of Interest"""
+        """Changes the difficulty of the Point of Interest.
+
+        Changing the difficulty to an invalid difficulty raises a TypeError."""
         assert False, "Not Yet Implemented"
