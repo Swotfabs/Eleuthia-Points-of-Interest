e04d54ec4749e5f23fdb368145f9e954e05f8716
Fabian Junge <fabsjunge@gmail.com>
2017-03-14 14:07:17 -0500

    added variable testing

--- unittesting.py
+++ unittesting.py
@@ -8,4 +8,20 @@ from UnitTests.points_of_interest_unittests import PointOfIntersetTests
 from UnitTests.player_unittests import PlayerTests
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description="Run Unit Tests")
+    parser.add_argument('--points', action='store_true', dest='pointTests',
+                        help="Runs the Point of Interest Unit Tests")
+    parser.add_argument('--players', action='store_true', dest='playerTests',
+                        help="Runs the Player Unit Tests")
+
+    args = parser.parse_args()
+    suite = unittest.TestSuite()
+
+    if not args.pointTests and not args.playerTests:
+        unittest.main()
+    if args.pointTests:
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(PointOfIntersetTests))
+    if args.playerTests:
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(PlayerTests))
+
+    unittest.TextTestRunner().run(suite)
